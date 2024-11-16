b39883f3537d46bf41af2b387d8ace2b68213e5c
Bandish Kumar <94bandishkumar@gmail.com>
2024-11-16 17:26:18 +0530

    Intial commit for swiftUI code

---
+++ Solid_Principles/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Solid_Principles
+//
+//  Created by Bandish Kumar on 16/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Solid_Principles/Solid_PrinciplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Solid_PrinciplesApp.swift
+//  Solid_Principles
+//
+//  Created by Bandish Kumar on 16/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Solid_PrinciplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
