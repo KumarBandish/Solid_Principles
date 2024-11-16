7f531c060b8d4f44c80a709e3339b54cb98f876a
Bandish Kumar <94bandishkumar@gmail.com>
2024-11-16 18:24:12 +0530

    Added User, AuthManager,UserProfileManager class to perfom single responsibility

--- Solid_Principles/ContentView.swift
+++ Solid_Principles/ContentView.swift
@@ -7,17 +7,44 @@
 
 import SwiftUI
 
+enum GenderType{
+    static let m = "Male"
+    static let f = "Female"
+}
 struct ContentView: View {
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            let userDetails = getUserData()
+            
+            ForEach(userDetails, id: \.name) { userDetail in
+                Label(title: {
+                    Text(userDetail.name)
+                        .font(.headline)
+                        .foregroundStyle(Color.black)
+                        .fontWeight(.bold)
+                        .font(.title2)
+                }, icon: {
+                    userDetail.gender == GenderType.m ?
+                    Image(systemName: "person.crop.circle.fill") :  Image(systemName: "person.crop.circle.fill.badge.checkmark")
+                   
+                        
+                })
+                .padding()
+                .fontWeight(.medium)
+                .foregroundColor(.red)
+                
+            }
+            .padding()
         }
-        .padding()
+    }
+    
+    func getUserData() -> [User] {
+        return [User(name: "Bandish", age: 30, gender: "Male"), User(name: "Shivani", age: 27, gender: "Female")]
     }
 }
+
+
 
 #Preview {
     ContentView()
